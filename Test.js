939cbdd3c03da6b660e5162a119fec71b7233119
Anthony Howl <anthonyhowl@protonmail.ch>
2018-07-05 12:40:25 -0700

    IPFS daemon kill on control + C

--- Test.js
+++ Test.js
@@ -4,3 +4,9 @@ var dl = new Downloader()
 
 dl.download("e0113b", __dirname + "/data")
 
+
+process.on('SIGINT', function() {
+    console.log("Shutting down IPFS Node");
+    dl.shutdown();
+    process.exit();
+});
