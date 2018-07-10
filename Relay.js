da7d61724dcefb130a84ea400090d16f08cbb9af
Anthony Howl <anthonyhowl@protonmail.ch>
2018-07-09 17:11:34 -0700

    creation of data directory

--- Relay.js
+++ Relay.js
@@ -1,8 +1,44 @@
 var Downloader = require('./lib/app.js');
+var pkjson = require('./package.json')
+var chalk = require('chalk')
+var figlet = require('figlet')
+const fs = require('fs-extra')
+
+var init = figlet.textSync(`OIP P2P Downloader`, {horizontalLayout: 'fitted'})
+
+console.log(chalk.grey(init))
+console.log(`\t\t\t\t\t\t\t  ${chalk.cyan("version " + pkjson.version)} ${chalk.hex("#FF6E1E")("by Anthony Howl from the OIPWG")}`)
+
+const store = this.__dirname + '/data';
+
+fs.ensureDir(store, err => {
+  console.log(err) // => null
+})
+
+fs.ensureDir(store)
+.then(() => {
+  console.log('success!')
+})
+.catch(err => {
+  console.error(err)
+})
+
+
+async function example (store) {
+  try {
+    await fs.ensureDir(store)
+    console.log('success!')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
 
 var dl = new Downloader()
 
-dl.download("e0113b", __dirname + "/data")
+dl.download("e0113b", __dirname + '/data')
+
 
 
 process.on('SIGINT', function() {
